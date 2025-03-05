92576a45a79ccfd906cefa51b8b86799305bdd9a
Dot-ser <alosiousbenny7@gmail.com>
2025-03-05 22:28:36 +0530

    Added Async ....

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")  
 PORT = int(os.getenv("PORT", 5000))
 # Configure logging with emojis
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - 💀<-🔫¸ %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - 💀🔫¸ %(message)s')
 
 bot_app = Client("Autoclr", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
@@ -33,18 +33,18 @@ def keep_alive():
 
 # Delete join messages
 @bot_app.on_message(filters.new_chat_members)
-def delete_join_message(client, message):
+async def delete_join_message(client, message):
     try:
-        message.delete()
+        await message.delete()
         logging.info(f"🗑️ Deleted join message in {message.chat.title}")
     except Exception as e:
         logging.error(f"⚠️ Error deleting join message: {e}")
 
 # Delete leave messages
 @bot_app.on_message(filters.left_chat_member)
-def delete_leave_message(client, message):
+async def delete_leave_message(client, message):
     try:
-        message.delete()
+        await message.delete()
         logging.info(f"🗑️ Deleted leave message in {message.chat.title}")
     except Exception as e:
         logging.error(f"⚠️ Error deleting leave message: {e}")
